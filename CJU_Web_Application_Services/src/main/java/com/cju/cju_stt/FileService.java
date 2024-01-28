173c560268cf330500831db77f5dd584b49ec1db
jongho22 <sjh0804_22@naver.com>
2024-01-28 20:21:56 +0900

    feat: publish server

--- CJU_Web_Application_Services/src/main/java/com/cju/cju_stt/FileService.java
+++ CJU_Web_Application_Services/src/main/java/com/cju/cju_stt/FileService.java
@@ -3,7 +3,6 @@ package com.cju.cju_stt;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.springframework.stereotype.Service;
@@ -17,59 +16,58 @@ public class FileService {
 		
 		String resultText;
 		String orgFileName = audioFile.getOriginalFilename();
-	    long fileSize = audioFile.getSize();
-	    String realPath = "/home/jongho/바탕화면/";
-	    String filePath = realPath+File.separator;
-	    
-	    File upload=new File(filePath);
-	      if(!upload.exists()) {  // 디렉토리가 존재하지 않으면 
-	         upload.mkdir();      
-	      }
-       String saveFileName = System.currentTimeMillis()+orgFileName;
+		String realPath = "/home/jongho/바탕화면/";
+		String filePath = realPath+File.separator;
+		String saveFileName = System.currentTimeMillis()+orgFileName;
        
        try {
     	   	  audioFile.transferTo(new File(filePath+saveFileName));
-	         System.out.println("[FileService] (Save File) => "+filePath+saveFileName);
-	         
+	         System.out.println("[FileService] (Save File SUCCESS) => " + filePath+saveFileName);
 	      }catch(Exception e) {
+	    	  System.out.println("[FileService] (Save File FAIL!) => " + saveFileName);
 	         e.printStackTrace();
 	      }
-		ProcessBuilder processBuilder = new ProcessBuilder("python3", "/home/jongho/바탕화면/test2.py", orgFileName);
+        
+		ProcessBuilder processBuilder = new ProcessBuilder("python3", "/home/jongho/바탕화면/main.py", saveFileName);
 		Process process;
 		
 		try {
 			process = processBuilder.start();
+			System.out.println("[FileService] (Start process) => " + saveFileName);
 			
 			try {
 			    int exitval = process.waitFor();
-			    InputStream inputStream = process.getInputStream();
+			    // InputStream inputStream = process.getInputStream();
 			    BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(),"UTF-8"));
-			    
-			    // Delete upload file
-			    File delFile = new File(filePath+saveFileName);
-			    
-			    if(delFile.exists()) {
-			    	delFile.delete();
-			    	System.out.println("[FileService] 파일삭제 성공");
-			    } else {
-			    	System.out.println("[FileService] 파일이 존재하지 않습니다.");
-			    }
 			    
 			    // Result data
 			    while ((resultText = br.readLine()) != null) {
-			    	System.out.println("[FileService] SUCCESS!!");
+			    	System.out.println("[FileService] (SUCCESS)");
 			    	return resultText;
 	     	    }
-			    System.out.println("[FileService] 문제가 발생하였습니다.");
+			    System.out.println("[FileService] (** FAIL **)");
 			    return "문제가 발생하였습니다.";
 			       
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 				return null;
-			}
+			} 
+			
 		} catch (IOException e) {
+			System.out.println("[FileService] Fail processBuilder start.");
 			e.printStackTrace();
 			return null;
+			
+		} finally {
+			// Delete upload file
+		    File delFile = new File(filePath+saveFileName);
+		    
+		    if(delFile.exists()) {
+		    	delFile.delete();
+		    	System.out.println("[FileService] (파일삭제 성공) => " + saveFileName);
+		    } else {
+		    	System.out.println("[FileService] (파일이 존재하지 않습니다.)");
+		    }
 		}
 	}
 }
