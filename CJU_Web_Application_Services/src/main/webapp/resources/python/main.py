f9fa16f0602e734a01fc18b07476acc187bec733
jongho22 <sjh0804_22@naver.com>
2024-02-29 22:27:06 +0900

    test: Processing voice files via Python

--- CJU_Web_Application_Services/src/main/webapp/resources/python/main.py
+++ CJU_Web_Application_Services/src/main/webapp/resources/python/main.py
@@ -2,6 +2,8 @@ import whisper
 import sys
 import torch
 import os
+from datetime import datetime
+import time
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -9,15 +11,16 @@ if __name__ == "__main__":
     model = whisper.load_model("medium")
     model = model.to(device)
 
-    file_name = sys.argv[1]
-    file_path = sys.argv[2]
+    file_name = "광화문자생한방병원_1.mp3"
+    file_path = "/home/jongho/바탕화면/테스트 음성파일/"
     
-    try :
-        result = model.transcribe(f"{file_path}{file_name}", fp16=False)
-        #print(os.getcwd())
-        print(result['text'])
-    except :
-        print("다른 작업으로 인해 서버가 과부하 되었습니다.")
-        #print("check" + os.getcwd())
-    
+
+    start = time.time()
+    result = model.transcribe(f"{file_path}{file_name}", fp16=False)
+    #print(os.getcwd())
+    print(result['segments'])
+    end = time.time()
+
+    print(start-end)
+
     
